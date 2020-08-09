3e81ab5a7612b1399c445b7475ab3d2d563d72b4
Alon Muroch <alonmuroch@gmail.com>
2020-08-09 14:09:27 +0300

    deposit 32 eth via blox stakaing contract

--- contracts/BloxStaking.sol
+++ contracts/BloxStaking.sol
@@ -2,13 +2,20 @@
 pragma solidity ^0.6.2;
 
 import 'openzeppelin-solidity/contracts/token/ERC20/ERC20.sol';
+import './DemoValidatorDeposit.sol';
 
 contract BloxStaking {
-    ERC20 public cdt;
+    address  public cdt;
+    address  public deposit_contract;
     uint counter;
 
-    constructor(ERC20 _cdt) public {
+    event DepositedValidator(bytes pubkey, bytes withdrawal_credentials, uint256 amount);
+    event DepositFailed(string error);
+    event FeePaid(bytes pubkey, uint256 cdt_amount);
+
+    constructor(address _cdt, address _deposit_contract) public {
         cdt = _cdt;
+        deposit_contract = _deposit_contract;
         counter = 0;
     }
 
@@ -18,5 +25,35 @@ contract BloxStaking {
 
     function getCount() public view returns (uint) {
         return counter;
+    }
+
+    function depositAndFee(
+        bytes calldata pubkey,
+        bytes calldata withdrawal_credentials,
+        bytes calldata signature,
+        bytes32 deposit_data_root,
+        uint256 fee_amount_eth
+    ) external payable {
+
+    }
+
+    function validatorDeposit(
+        bytes memory pubkey,
+        bytes memory withdrawal_credentials,
+        bytes memory signature,
+        bytes32 deposit_data_root
+    ) public payable {
+        require(msg.value > 32 ether, "insufficient funds");
+
+        try DepositContract(deposit_contract).deposit.value(32 ether)(pubkey, withdrawal_credentials, signature, deposit_data_root) {
+            emit DepositedValidator(pubkey, withdrawal_credentials, 32 ether);
+        } catch Error(string memory _err) {
+            emit DepositFailed(_err);
+            return;
+        }
+    }
+
+    function payFee(uint256 fee_amount_eth) public payable {
+
     }
 }
--- test/InitialDepositTest.js
+++ test/InitialDepositTest.js
@@ -1,4 +1,6 @@
+const Web3 = require('web3');
 const bloxStaking = artifacts.require("BloxStaking");
+const depositContract = artifacts.require("DepositContract");
 
 contract("BloxStaking - initial deposit", async accounts => {
     it("should get counter", async () => {
@@ -6,6 +8,33 @@ contract("BloxStaking - initial deposit", async accounts => {
         let count = await instance.getCount.call();
         assert.equal(count.valueOf(), 0);
     });
+
+    it("test simple deposit", async () => {
+        let instance = await bloxStaking.deployed();
+
+        const decimals = Web3.utils.toBN(10).pow(Web3.utils.toBN(18));
+        value = Web3.utils.toBN(33).mul(decimals);
+
+        return instance
+            .validatorDeposit
+            .sendTransaction(
+                web3.utils.hexToBytes("0xa70360aa20eb09a00019e47c9775ba8e655b934b86de409a49a25e4196663c5f10dffe29b12b7cf782e4561a43c5c62c"),
+                web3.utils.hexToBytes("0x00c76a029adcac82fe161b34f44de3c8c94182ffe75bf29a938691ebfd66bf6b"),
+                web3.utils.hexToBytes("0x88ff6c5a44b85db96b684cee772506489ae388838fe4d13435bf415de23ce14a9b4f254dd1f456cffbb581d87f4a6ce806f559e8d1afa28cdbde84a5fba6526e9f948ddde7166d8ba8218478e5e681833492d61a7b49d11ced0718ac317218df"),
+                web3.utils.hexToBytes("0x6255505dc4c2ba5828cc6ad8f47bd122f02d8c840fc1aa81abd817f3971c2d79"),
+                {from: accounts[1], value: value}
+            )
+            .then(res => {
+                console.log(res);
+                res.receipt.logs.forEach(function(v,i,arr) {
+                    console.log(v);
+                });
+            })
+            .catch (e => {
+                console.log(e);
+            })
+    });
+
 
     // it("should call a function that depends on a linked library", async () => {
     //     let meta = await MetaCoin.deployed();
