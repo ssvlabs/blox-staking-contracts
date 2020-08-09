b39341d5827f9b540e0a9b741462ebdd4d32b1bc
Alon Muroch <alonmuroch@gmail.com>
2020-08-09 18:44:18 +0300

    readme + total staked

--- contracts/BloxStaking.sol
+++ contracts/BloxStaking.sol
@@ -8,11 +8,13 @@ import './TestExchangeFactory.sol';
 import './CDT.sol';
 
 contract BloxStaking {
-    address  public cdt;
-    address  public deposit_contract;
-    address  public exchange;
+    address public cdt;
+    address public deposit_contract;
+    address public exchange;
 
-    event DepositedValidator(bytes pubkey, bytes withdrawal_credentials, uint256 amount);
+    uint256 internal total_staked;
+
+    event DepositedValidator(bytes pubkey, bytes withdrawal_credentials, uint256 amount, uint256 updated_total_staked);
     event DepositFailed(string error);
     event FeeBurned(uint256 cdt_amount);
     event FeePurchaseFailed(string error);
@@ -21,6 +23,12 @@ contract BloxStaking {
         cdt = _cdt;
         deposit_contract = _deposit_contract;
         exchange = _exchange;
+
+        total_staked = 0;
+    }
+
+    function getTotalStaked() external view returns (uint256) {
+        return total_staked;
     }
 
     function depositAndFee(
@@ -44,11 +52,15 @@ contract BloxStaking {
         require(msg.value >= 32 ether, "insufficient funds");
 
         try DepositContract(deposit_contract).deposit{value: 32 ether}(pubkey, withdrawal_credentials, signature, deposit_data_root) {
-            emit DepositedValidator(pubkey, withdrawal_credentials, 32 ether);
+            // update total staked
+            total_staked += 32 ether;
+
+            emit DepositedValidator(pubkey, withdrawal_credentials, 32 ether, total_staked);
         } catch Error(string memory _err) {
             emit DepositFailed(_err);
             return;
         }
+
     }
 
     function payFeeInETH(uint256 fee_amount_eth) public payable {
