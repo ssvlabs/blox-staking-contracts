cfe56616b90b4e2ff09fabcb3842ea86ec18d2a5
roman <roman@blox.io>
2020-10-12 12:46:49 +0300

    added tests

--- test/depositTest.js
+++ test/depositTest.js
@@ -108,6 +108,81 @@ contract("Deposit testing", async accounts => {
         expect(res.toString()).to.have.string("Error: Returned error: VM Exception while processing transaction: revert");
     });
 
+    it("invalid deposit - invalid withdrawal creds", async () => {
+        let instance = await bloxStaking.deployed();
+
+        const decimals = Web3.utils.toBN(10).pow(Web3.utils.toBN(18));
+        value = Web3.utils.toBN(32).mul(decimals);
+
+        let res;
+        try {
+            res = await instance
+                .validatorDeposit
+                .sendTransaction(
+                    web3.utils.hexToBytes("0xa70360aa20eb09a00019e47c9775ba8e655b934b86de409a49a25e4196663c5f10dffe29b12b7cf782e4561a43c5c62c"),
+                    web3.utils.hexToBytes("0xa70360aa20eb09a00019e47c9775ba8e655b934b86de409a49a25e4196663c5f10dffe29b12b7cf782e4561a43c5c62c"), // invalid
+                    web3.utils.hexToBytes("0x88ff6c5a44b85db96b684cee772506489ae388838fe4d13435bf415de23ce14a9b4f254dd1f456cffbb581d87f4a6ce806f559e8d1afa28cdbde84a5fba6526e9f948ddde7166d8ba8218478e5e681833492d61a7b49d11ced0718ac317218df"),
+                    web3.utils.hexToBytes("0x6255505dc4c2ba5828cc6ad8f47bd122f02d8c840fc1aa81abd817f3971c2d79"),
+                    {from: accounts[1], value: value}
+                );
+        }
+        catch (err) {
+            res = err;
+        }
+        expect(res).to.be.instanceOf(Error);
+        expect(res.toString()).to.have.string("Error: Returned error: VM Exception while processing transaction: revert");
+    });
+
+    it("invalid deposit - invalid signature", async () => {
+        let instance = await bloxStaking.deployed();
+
+        const decimals = Web3.utils.toBN(10).pow(Web3.utils.toBN(18));
+        value = Web3.utils.toBN(32).mul(decimals);
+
+        let res;
+        try {
+            res = await instance
+                .validatorDeposit
+                .sendTransaction(
+                    web3.utils.hexToBytes("0xa70360aa20eb09a00019e47c9775ba8e655b934b86de409a49a25e4196663c5f10dffe29b12b7cf782e4561a43c5c62c"),
+                    web3.utils.hexToBytes("0x00c76a029adcac82fe161b34f44de3c8c94182ffe75bf29a938691ebfd66bf6b"),
+                    web3.utils.hexToBytes("0x00c76a029adcac82fe161b34f44de3c8c94182ffe75bf29a938691ebfd66bf6b"), // invalid
+                    web3.utils.hexToBytes("0x6255505dc4c2ba5828cc6ad8f47bd122f02d8c840fc1aa81abd817f3971c2d79"),
+                    {from: accounts[1], value: value}
+                );
+        }
+        catch (err) {
+            res = err;
+        }
+        expect(res).to.be.instanceOf(Error);
+        expect(res.toString()).to.have.string("Error: Returned error: VM Exception while processing transaction: revert");
+    });
+
+    it("invalid deposit - wrong signature", async () => {
+        let instance = await bloxStaking.deployed();
+
+        const decimals = Web3.utils.toBN(10).pow(Web3.utils.toBN(18));
+        value = Web3.utils.toBN(32).mul(decimals);
+
+        let res;
+        try {
+            res = await instance
+                .validatorDeposit
+                .sendTransaction(
+                    web3.utils.hexToBytes("0xa70360aa20eb09a00019e47c9775ba8e655b934b86de409a49a25e4196663c5f10dffe29b12b7cf782e4561a43c5c62c"),
+                    web3.utils.hexToBytes("0x00c76a029adcac82fe161b34f44de3c8c94182ffe75bf29a938691ebfd66bf6b"),
+                    web3.utils.hexToBytes("0x89d1ffb6bb53393c408656a05fd8f148eef5985d14b5e09d8e1895e320a40d4ff2718558a0d00c55c43e8eba83a392180aea738feedc2d99e8cee7fe5f9e10813a5d60f6a8dbd8b8ea180aa90acc7910bdc6b9a8389aafa85d7c27d3d335117d"), // wrong
+                    web3.utils.hexToBytes("0x6255505dc4c2ba5828cc6ad8f47bd122f02d8c840fc1aa81abd817f3971c2d79"),
+                    {from: accounts[1], value: value}
+                );
+        }
+        catch (err) {
+            res = err;
+        }
+        expect(res).to.be.instanceOf(Error);
+        expect(res.toString()).to.have.string("Error: Returned error: VM Exception while processing transaction: revert");
+    });
+
     it("invalid deposit - wrong root", async () => {
         let instance = await bloxStaking.deployed();
 
